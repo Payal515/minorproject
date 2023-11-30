20a8ff7b80c2c15605cd0c8c71f5500aa31e1beb
Payal515 <payalsolanki76389@gmail.com>
2023-11-30 16:42:28 +0530

    first comit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import {NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+ const routes: Routes=[
+
+    {path:'home', component: HomeComponent},
+    {path:'admin', component: AdminComponent},
+    {path:'user', component: UserComponent},
+    {path:'login', component: LoginComponent},
+
+    {path:'forbidden', component: ForbiddenComponent}
+   
+   
+  
+  
+];
+  
+
+@NgModule({
+  imports:[RouterModule.forRoot(routes)],
+  exports:[RouterModule]
+})
+export class AppRoutingModule{
+
+}
+
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { UserAuthService } from '../_services/user-auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent  implements OnInit{
+  constructor(private userAuthService:UserAuthService,
+    private router:Router) {}
+  ngOnInit(): void {}
+    
+    public isLoggedIn(){
+      return this.userAuthService.isLoggedIn();
+    }
+    public logout(){
+      this.userAuthService.clear();
+      this.router.navigate(['/home']);
+    }
+  }
+
